3fc27a2dc90fb49a17138f673273fc39c688584d
Oleksandr Yatsiuta <y.alexandr.32@gmail.com>
2025-08-03 20:34:24 +0300

    fix and rework search

--- js/search.js
+++ js/search.js
@@ -1,36 +1,105 @@
 let area = document.querySelector(".search-area");
 let search = document.querySelector(".search");
-let all = document.querySelector(".all-elements");
 let icon = document.querySelector(".fa-search");
+let noResults = document.querySelector(".no-results");
 
-icon.onclick = function () {
-	area.style.display = "flex";
+// Функція для відкриття пошуку
+function openSearch() {
+    if (area) {
+        area.style.display = "flex";
+        // Показуємо всі елементи по замовчуванню
+        let elasticItems = document.querySelectorAll(".elastic li");
+        elasticItems.forEach(function (elem) {
+            elem.classList.remove("hide");
+        });
+        // Приховуємо повідомлення про відсутність результатів
+        if (noResults) {
+            noResults.classList.add("hide");
+        }
+    }
 }
 
-search.onclick = function () {
-	area.style.display = "flex";
+// Функція для закриття пошуку
+function closeSearch() {
+    if (area) {
+        area.style.display = "none";
+    }
 }
 
-all.onclick = function () {
-	area.style.display = "none";
+// Відкриття пошуку при кліку на іконку
+if (icon) {
+    icon.onclick = function () {
+        openSearch();
+    }
 }
 
-document.querySelector("#elastic").oninput = function () {
-	let val = this.value.trim();
-	let elasticItems = document.querySelectorAll(".elastic li");
-	if (val != '') {
-		elasticItems.forEach(function (elem) {
-			if (elem.innerText.search(val) == -1) {
-				elem.classList.add("hide");
-			}
-			else {
-				elem.classList.remove("hide");
-			}
-		});
-	}
-	else {
-		elasticItems.forEach(function (elem) {
-			elem.classList.remove("hide");
-		});
-	}
+// Відкриття пошуку при кліку на поле вводу
+if (search) {
+    search.onclick = function () {
+        openSearch();
+    }
+}
+
+// Закриття пошуку при кліку на документ (поза областю пошуку)
+document.addEventListener('click', function(event) {
+    if (area && area.style.display === "flex") {
+        // Перевіряємо, чи клік був поза областю пошуку
+        if (!area.contains(event.target) && 
+            !search.contains(event.target) && 
+            !icon.contains(event.target)) {
+            closeSearch();
+        }
+    }
+});
+
+// Закриття пошуку при натисканні Escape
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeSearch();
+        if (search) {
+            search.value = "";
+            search.blur();
+        }
+    }
+});
+
+// Фільтрація результатів пошуку
+let elasticInput = document.querySelector("#elastic");
+if (elasticInput) {
+    elasticInput.oninput = function () {
+        let val = this.value.trim();
+        let elasticItems = document.querySelectorAll(".elastic li");
+        let visibleItems = 0;
+        
+        if (val != '') {
+            elasticItems.forEach(function (elem) {
+                if (elem.innerText.toLowerCase().search(val.toLowerCase()) == -1) {
+                    elem.classList.add("hide");
+                }
+                else {
+                    elem.classList.remove("hide");
+                    visibleItems++;
+                }
+            });
+            
+            // Показуємо або приховуємо повідомлення про відсутність результатів
+            if (noResults) {
+                if (visibleItems === 0) {
+                    noResults.classList.remove("hide");
+                } else {
+                    noResults.classList.add("hide");
+                }
+            }
+        }
+        else {
+            elasticItems.forEach(function (elem) {
+                elem.classList.remove("hide");
+            });
+            
+            // Приховуємо повідомлення про відсутність результатів, коли поле порожнє
+            if (noResults) {
+                noResults.classList.add("hide");
+            }
+        }
+    }
 }
